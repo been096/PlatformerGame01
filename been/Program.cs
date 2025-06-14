636290183f022ae7dfa02ecda3a4f4ea5bc75e84
been <been0562@naver.com>
2025-06-14 23:54:13 +0900

    복습한것.

---
+++ been/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace been
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! 박원빈입니다");
+            Console.WriteLine("Add:" + Add(10, 20));
+            ValMain();
+            CritcalAttackMain();
+            StageMain();
+            AttackWhile();
+            MonsterListMain();
+        }
+
+        static int Add(int a, int b)//10,20
+        {
+            int c = a + b; // 10 + 20 = 30
+            return c; //30
+        }
+
+
+        static void ValMain()
+        {
+            int nScore = 0;
+            float fRat = 1.0f / 4.0f;
+            Console.WriteLine("Score:" + nScore);
+            Console.WriteLine("Rat:" + fRat);
+        }
+
+        static void CritcalAttackMain()
+        {
+            Console.WriteLine("CritcalAttackMain");
+        }
+
+        static void StageMain()
+        {
+            Console.WriteLine("StageMain");
+        }
+
+        static void AttackWhile()
+        {
+            Console.WriteLine("AttackWhile");
+        }
+
+        static void MonsterListMain()
+        {
+            Console.WriteLine("MonsterListMain");
+        }
+    }
+}
